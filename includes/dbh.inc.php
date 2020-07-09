89799a1229d3073f4db3e55afe4d92e6bec78bf0
Connor Sharp <cjsharp99@gmail.com>
2020-07-08 23:32:48 -0500

    updated db file to connect to aws rds

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,12 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsystemtext";
+$servername = "testdb.cnx7v6bxswzq.us-east-1.rds.amazonaws.com";
+$dBUsername = "admin";
+$dBPassword = "TextmateAdmin2020";
+$dBName = "innodb";
+$dBPort = "3306";
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, $dBPort);
 
 if(!$conn) {
     die("Connection failed: " .mysqli_connect_error());
